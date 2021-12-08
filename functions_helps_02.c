c00d12464d463d675ed2ac2b0409d68b017dad62
MiguelBarreraDev <barreram987@gmail.com>
2021-12-08 09:48:13 -0500

    Add to_str function

--- functions_helps_02.c
+++ functions_helps_02.c
@@ -100,3 +100,31 @@ void _concat(char **buff, int n, ...)
 	free(arr);
 	va_end(ptr);
 }
+/**
+ * to_str - convert int to string
+ * @n: number to be converted into a string
+ * Return: pointer to a new string
+ */
+char *to_str(int n)
+{
+	char *str, *p;
+	int n2 = n, i = 0;
+
+	while (n2 > 0)
+	{
+		i++;
+		n2 /= 10;
+	}
+	str = malloc(sizeof(char) * (i + 1));
+	str[i] = '\0';
+
+	p = &str[i];
+	p--;
+	while (n > 0)
+	{
+		*p = n % 10 + '0';
+		n /= 10;
+		p--;
+	}
+	return (str);
+}
