6a09a4ccb5849970cb5c7168230bead09b0c2cdf
MiguelBarreraDev <barreram987@gmail.com>
2021-12-08 10:28:22 -0500

    Add init_struct function

--- shell.c
+++ shell.c
@@ -16,19 +16,16 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 	char *prompt = ":) ";
 	int r_getline = 0, i = 0;
 
-	pmt.command = NULL;
-	pmt.name_shell = argv[0];
-	pmt.environment = env;
-	pmt.band = 0;
-	pmt.lines = 0;
-	/* info - IMPORT! reserved memory */
-	pmt.tokens = malloc(sizeof(char *) * 50);
+	init_struct(&pmt, argv, env);
 	signal(SIGINT, shell_signal);
 	while (1)
 	{
 		/* info - Check the file descriptor reference */
 		if (isatty(STDIN_FILENO))
+		{
 			write(STDOUT_FILENO, prompt, strlen(prompt));
+			pmt.input_type = 1;
+		}
 		pmt.lines += 1;
 		r_getline = getline(&(pmt.command), &sz_buffer, stdin);
 		/* info - Exit my shell options "crtl + D" */
@@ -51,4 +48,21 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 	if (pmt.band == 1)
 		_free(pmt.environment);
 	return (0);
+}
+/**
+ * init_struct - function that initializing Parameters structure
+ * @pmt: pointer to Parameters structure
+ * @argv: pointer to list of arguments of the main functions
+ * @env: pointer to memory space of the environment variables
+ */
+void init_struct(st_parameters *pmt, char **argv, char **env)
+{
+	pmt->command = NULL;
+	pmt->name_shell = argv[0];
+	pmt->environment = env;
+	pmt->band = 0;
+	pmt->lines = 0;
+	pmt->input_type = 0;
+	/* info - IMPORT! reserved memory */
+	pmt->tokens = malloc(sizeof(char *) * 50);
 }
