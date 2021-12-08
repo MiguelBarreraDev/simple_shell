8ab44c373284943f0f56803bebf24d6ca68b00db
MiguelBarreraDev <barreram987@gmail.com>
2021-12-08 05:40:22 -0500

    Add isatty function

--- shell.c
+++ shell.c
@@ -26,10 +26,10 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 	while (1)
 	{
 		/* info - Check the file descriptor reference */
-		/**
-		 *if (isatty(STDIN_FILENO))
-		 *	write(STDOUT_FILENO, prompt, strlen(prompt));
-		 */
+
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, prompt, strlen(prompt));
+
 		r_getline = getline(&(pmt.command), &sz_buffer, stdin);
 		/* info - Exit my shell options "crtl + D" */
 		if (r_getline == EOF)
