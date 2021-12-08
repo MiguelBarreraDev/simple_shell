8d4e9974a65f6e98daf29a16d705442bdc2215dd
MiguelBarreraDev <barreram987@gmail.com>
2021-12-08 05:59:37 -0500

    remove _trim function

--- shell.c
+++ shell.c
@@ -14,7 +14,7 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 	st_parameters pmt;
 	size_t sz_buffer;
 	char *prompt = ":) ";
-	int r_getline, i;
+	int r_getline = 0, i = 0;
 
 	pmt.command = NULL;
 	pmt.name_shell = argv[0];
@@ -23,18 +23,17 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 	/* info - IMPORT! reserved memory */
 	pmt.tokens = malloc(sizeof(char *) * 50);
 	/*signal(SIGINT, shell_signal);*/
-	while (1)
+	while (r_getline != -1)
 	{
 		/* info - Check the file descriptor reference */
-
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, prompt, strlen(prompt));
 		r_getline = getline(&(pmt.command), &sz_buffer, stdin);
 		/* info - Exit my shell options "crtl + D" */
-		if (r_getline == EOF)
-			break;
 		/* info - Remove blanks at start */
-		_trim(&(pmt.command));
+		/*_trim(&(pmt.command));*/
 		/* info - Controlling line break "\n" */
-		if (strlen(pmt.command) <= 1)
+		if (r_getline <= 1)
 			continue;
 
 		for (i = 0; (pmt.tokens[i] = my_strtok(pmt.command, " \n")) != NULL; i++)
