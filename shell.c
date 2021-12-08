70b9b9bf74fd50a3316e155567a3be22275cbf81
MiguelBarreraDev <barreram987@gmail.com>
2021-12-08 06:17:09 -0500

    Add signal and isatty functions

--- shell.c
+++ shell.c
@@ -14,7 +14,7 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 	st_parameters pmt;
 	size_t sz_buffer;
 	char *prompt = ":) ";
-	int r_getline = 0, i;
+	int r_getline = 0, i = 0;
 
 	pmt.command = NULL;
 	pmt.name_shell = argv[0];
@@ -22,8 +22,8 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 	pmt.band = 0;
 	/* info - IMPORT! reserved memory */
 	pmt.tokens = malloc(sizeof(char *) * 50);
-	/*signal(SIGINT, shell_signal);*/
-	while (r_getline != -1)
+	signal(SIGINT, shell_signal);
+	while (1)
 	{
 		/* info - Check the file descriptor reference */
 		if (isatty(STDIN_FILENO))
@@ -35,7 +35,7 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 		/* info - Remove blanks at start */
 		_trim(&(pmt.command));
 		/* info - Controlling line break "\n" */
-		if (r_getline <= 1)
+		if (strlen(pmt.command) <= 1)
 			continue;
 
 		for (i = 0; (pmt.tokens[i] = my_strtok(pmt.command, " \n")) != NULL; i++)
