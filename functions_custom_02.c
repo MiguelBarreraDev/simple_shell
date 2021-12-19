bddda4d247b2a96cbd257d9fafe0e3c0192f207d
MiguelBarreraDev <barreram987@gmail.com>
2021-12-19 13:55:51 -0500

    Refactoring code - shell_cd function

--- functions_custom_02.c
+++ functions_custom_02.c
@@ -1,6 +1,4 @@
 #include "shell.h"
-#include <linux/limits.h>
-#include <limits.h>
 /**
  * shell_signal - function that controls program interruption
  * @sig: integer number
@@ -11,34 +9,12 @@ void shell_signal(int sig)
 	write(STDOUT_FILENO, "\n:) ", 4);
 }
 /**
- * change_path - update PWD and OLDPWD environment variables
- * @pmt: pointer to structure with members related to user input
- * @pwd: array containing the new value of the environment variable PWD
- * @old: array containing the new value of the environment variable OLDPWD
- */
-void change_path(st_parameters *pmt, char pwd[PATH_MAX], char old[PATH_MAX])
-{
-	char *message = "No such file or directory\n";
-
-	if (chdir(pwd) == -1)
-		write(STDERR_FILENO, message, strlen(message));
-	else
-	{
-		pmt->tokens[1] = "PWD";
-		pmt->tokens[2] = pwd;
-		shell_setenv(pmt);
-		pmt->tokens[1] = "OLDPWD";
-		pmt->tokens[2] = old;
-		shell_setenv(pmt);
-	}
-}
-/**
  * shell_cd - función que cambia el directorio de trabajo actual
  * @pmt: pointer to structure with members related to user input
  */
 void shell_cd(st_parameters *pmt)
 {
-	char path_dest[PATH_MAX] = "", path_old[PATH_MAX] = "", *save;
+	char path_old[PATH_MAX] = "", *save, path_dest[PATH_MAX] = "";
 
 	getcwd(path_old, sizeof(path_old));
 	/* info - search path home in environment */
@@ -58,12 +34,7 @@ void shell_cd(st_parameters *pmt)
 	}
 	else
 	{
-		if (*(pmt->tokens[1]) != '/')
-			getcwd(path_dest, sizeof(path_dest));
-		strcat(path_dest, "/");
-		strcat(path_dest, pmt->tokens[1]);
-		if (path_dest[0] != '\0')
-			change_path(pmt, path_dest, path_old);
+		shell_cd_arg(pmt, path_dest, path_old);
 	}
 }
 /**
